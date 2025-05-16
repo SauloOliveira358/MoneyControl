71dae9a79a4d7dc26389a50b5a0333e0141c5782
SauloOliveira358 <saulooliveiram358@gmail.com>
2025-05-15 22:12:47 -0300

    Activit Adicionar gasto

---
+++ app/src/main/java/com/example/mcontrol/Adicionar_Gastos.java
@@ -0,0 +1,13 @@
+package com.example.mcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Adicionar_Gastos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_gastos);
+    }
+}
